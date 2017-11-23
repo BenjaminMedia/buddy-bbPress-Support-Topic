112394bf19e881814e5534a74ae01211d31d955d
Alf Henderson <alfhenderson@gmail.com>
2017-11-23 20:34:20 +0100

    Added composer.json file and bumped plugin version

--- buddy-bbpress-support-topic.php
+++ buddy-bbpress-support-topic.php
@@ -6,7 +6,7 @@
  * @package     Buddy_bbPress_Support_Topic
  * @author      G3ronim0
  * @link        https://github.com/G3ronim0/buddy-bbPress-Support-Topic
- * @version     2.1.4
+ * @version     2.1.5
  *
  * @copyright   2017 G3ronim0
  * @license     http://creativecommons.org/licenses/GPL/2.0/ GNU General Public License, version 2 or higher
